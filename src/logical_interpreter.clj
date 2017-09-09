9c9dc521725994cf26ceafacc24896960db45a52
AlfaroMiguel <miguel.alfaro.a95@gmail.com>
2017-09-09 18:31:02 -0300

    Add interpreter

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,7 +1,17 @@
-(ns logical-interpreter)
+(ns logical-interpreter
+  (:require [clojure.test :refer :all]
+    :use [database :only [generate-database]]
+    :use [facts :only [exist-fact evaluate-fact is-valid-fact?]]
+    :use [rules :only [exist-rule evaluate-rule]])
+  )
 
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
   [database query]
-  nil)
+  (let [database (generate-database database)]
+    (cond
+      (or (= nil database) (not (is-valid-fact? query))) nil
+      (exist-fact database query) (evaluate-fact database query)
+      (exist-rule database query) (evaluate-rule database query)
+      )))
