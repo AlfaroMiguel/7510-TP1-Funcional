f6816f2a569d21c3db9b448e268d85ec28236aa3
AlfaroMiguel <miguel.alfaro.a95@gmail.com>
2017-09-10 19:02:25 -0300

    Database documentation

--- src/database.clj
+++ src/database.clj
@@ -3,30 +3,25 @@
     :use [rules :only [is-valid-rule?]])
   )
 
-(defn is-valid-database?
-  "Devuelve true si la database es valida, false por el contrario"
-  [database]
-  (reduce =
-          (map (fn [x] (or (is-valid-fact? x) (is-valid-rule? x))) database)))
-
 (defn parse-raw-database
-  "Recibe una base de datos con facts y rules y devuelve un set con los facts y rules parseados.
-
-  Ejemplo base de datos:
-  varon(juan).
-  varon(pedro).
-  hijo(X, Y) :- varon(X), padre(Y, X)."
+  "Given a raw database as a string, returns a set of facts and/or generics rules"
   [raw-database]
   (set
    (filter
     (fn [x] (not (clojure.string/blank? x) ))
     (map
-     (fn [string] (clojure.string/trim-newline (clojure.string/trim string)) )
+     (fn [string] (clojure.string/trim string) )
      (clojure.string/split raw-database  #"\.")
      ))))
 
+(defn is-valid-database?
+  "Checks if the database contains valid rules or facts. Otherwise return false"
+  [database]
+  (reduce =
+          (map (fn [x] (or (is-valid-fact? x) (is-valid-rule? x))) database)))
+
 (defn generate-database
-  ""
+  "Given a raw database as a string, returns a ArrayMap with all the facts and rules"
   [raw-database]
   (let [parsed-database (parse-raw-database raw-database)]
     (if (not (is-valid-database? parsed-database)) nil
