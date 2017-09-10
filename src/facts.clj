a1c9cfaf76c0358c5e239b5333bc73664d6f6828
AlfaroMiguel <miguel.alfaro.a95@gmail.com>
2017-09-10 19:51:34 -0300

    Facts documentation

--- src/facts.clj
+++ src/facts.clj
@@ -1,38 +1,37 @@
 (ns facts)
 
 (defn is-valid-fact?
-  "Chequea si es un fact es valido usando regex"
+  "Given a fact checks if it's valid"
   [fact]
   (not (= nil (re-matches #"^[a-z]+\(([a-z]+, )*[a-z]+\)$" fact))))
 
 (defn fact-name
-  "Devuelve el nombre de un fact"
+  "Given a fact, returns his name"
   [fact]
   (subs fact 0 (clojure.string/index-of fact "(")))
 
 (defn exist-fact
-  "Chequea que sea el nombre de una fact devuelve true si es asi"
+  "Given a database and a query, checks whether the query corresponds to the name of a fact in the database"
   [database query]
   (let [facts-names (map fact-name (:facts database))]
     (.contains facts-names (fact-name query))
     ))
 
 (defn evaluate-fact
-  "Recibe una db y una query (potencialmente fact) y se fija si existe en la base de datos de facts, si existe es verdadera y devuelvo true"
+  "Receives a database and a fact, it checks if all the fact (with its arguments) is in the database."
   [database fact]
   (.contains (:facts database) fact)
   )
 
 (defn replace-arg
-  ""
+  "Receives a generic fact e.g 'parent (Y, X)', a map with the values ​​of the arguments e.g '{X maría, Y roberto}'
+   and replaces the values. It returns the particular fact e.g 'padre(roberto, maría)'"
   [fact map-args]
   (clojure.string/replace fact (re-pattern (clojure.string/join "|" (keys map-args))) map-args)
   )
 
 (defn replace-args
-  "Recibe una lista con facts con argumentos genericos
-  y un map con los valores de esos argumentos, devuelve una lista con los facts con
-  los argumentos reemplazados"
+  "Receives a list of generic facts and a map with the values of the arguments, return a list of particulars facts"
   [gen-facts map-args]
   (map #( replace-arg % map-args ) gen-facts)
   )
